42e5884b5c7975e98971f20a3b87f6a5d0ff1fee
andresfgm84 <andresfgm84@users.noreply.github.com>
2021-08-11 16:56:51 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.f91b6a3fa306cbc96bb08128ee4e5b9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbad2048e9cd704550e6b7959bc2395",
+    "url": "https://andresfgm84.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "a14328f00998ecf78721",
+    "url": "https://andresfgm84.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "bd0422126a2519b88101",
+    "url": "https://andresfgm84.github.io/task-management-frontend/static/js/2.b7b09965.chunk.js"
+  },
+  {
+    "revision": "a14328f00998ecf78721",
+    "url": "https://andresfgm84.github.io/task-management-frontend/static/js/main.836fdbcf.chunk.js"
+  },
+  {
+    "revision": "c02b274719762a2af099",
+    "url": "https://andresfgm84.github.io/task-management-frontend/static/js/runtime~main.99c54a14.js"
+  }
+]);
